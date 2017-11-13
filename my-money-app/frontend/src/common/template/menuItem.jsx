7403aa9aee3f699bb31573918afd78e873a74228
Cristiano Dini <cristianodp@gmail.com>
2017-11-12 23:25:47 -0200

    Adição do login na aplicação my-money-app

--- my-money-app/frontend/src/common/template/menuItem.jsx
+++ my-money-app/frontend/src/common/template/menuItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default props => (
     <li>
-        <a href={props.path}>
+
+        <Link to={props.path}>
             <i className={`fa fa-${props.icon}`}/> <span> {props.label}</span>
-        </a>
+        </Link>
     
     </li>
 )
